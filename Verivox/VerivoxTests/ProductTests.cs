bc1fa81e849596d6452e9abcbefe5228fb5dc7d7
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-12-31 10:12:07 +0300

    Added test project

---
+++ Verivox/VerivoxTests/ProductTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Verivox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verivox.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void GetFixedAnnualCost_ProductA()
+        {
+            ProductA productA = new ProductA();
+            Assert.AreEqual(60, productA.GetFixedAnnualCost());
+        }
+
+        [TestMethod()]
+        public void GetFixedAnnualCost_ProductB()
+        {
+            ProductB productB = new ProductB();
+            Assert.AreEqual(800, productB.GetFixedAnnualCost());
+        }
+
+        [TestMethod()]
+        public void GetVariableAnnualCost_ProductA_Consumption3500()
+        {
+            ProductA productA = new ProductA();
+            decimal consumption = 3500;
+            Assert.AreEqual(770M, productA.GetVariableAnnualCost(consumption));
+        }
+
+        [TestMethod()]
+        public void GetVariableAnnualCost_ProductB_Consumption3500()
+        {
+            ProductB productB = new ProductB();
+            decimal consumption = 3500;
+            Assert.AreEqual(0, productB.GetVariableAnnualCost(consumption));
+        }
+
+        [TestMethod()]
+        public void GetVariableAnnualCost_ProductB_Consumption4500()
+        {
+            ProductB productB = new ProductB();
+            decimal consumption = 4500;
+            Assert.AreEqual(150, productB.GetVariableAnnualCost(consumption));
+        }
+
+        [TestMethod()]
+        public void GetTotalAnnualCosts_ProductA_Consumption3500()
+        {
+            ProductA productA = new ProductA();
+            decimal consumption = 3500;
+            Assert.AreEqual(830M, productA.GetTotalAnnualCosts(consumption));
+        }
+
+        [TestMethod()]
+        public void GetTotalAnnualCosts_ProductB_Consumption3500()
+        {
+            ProductB productB = new ProductB();
+            decimal consumption = 3500;
+            Assert.AreEqual(800M, productB.GetTotalAnnualCosts(consumption));
+        }
+    }
+}
---
+++ Verivox/VerivoxTests/TariffModelTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Verivox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verivox.Tests
+{
+    [TestClass()]
+    public class TariffModelTests
+    {
+        public const string productA_Name = "basic electricity tariff";
+        public const string productB_Name = "Packaged tariff";
+
+        private TariffModel InitializeTariffModel()
+        {
+            TariffModel model = new TariffModel();
+            ProductA productA = new ProductA(productA_Name);
+            ProductB productB = new ProductB(productB_Name);
+            model.AddProduct(productA);
+            model.AddProduct(productB);
+            return model;
+        }
+
+        [TestMethod()]
+        public void GetOffers_ThrowsArgumentException_IfConsumptionIsLowerThanZero()
+        {
+            decimal consumption = -100;
+            TariffModel model = new TariffModel();
+            var ex = Assert.ThrowsException<ArgumentException>(() => model.GetOffers(consumption));
+            Assert.AreEqual("Consumption should be non-negative", ex.Message);
+        }
+
+        [TestMethod()]
+        public void GetOffers_ProductBIsTheBestOffer_IfConsumptionIs3500()
+        {
+            decimal consumption = 3500;
+            var model = InitializeTariffModel();
+            var offers = model.GetOffers(consumption);
+
+            var bestOffer = offers[0];
+            Assert.AreEqual(productB_Name, bestOffer.Name);
+        }
+
+        [TestMethod()]
+        public void GetOffers_ProductBIsTheBestOffer_IfConsumptionIs4500()
+        {
+            decimal consumption = 4500;
+            var model = InitializeTariffModel();
+            var offers = model.GetOffers(consumption);
+
+            var bestOffer = offers[0];
+            Assert.AreEqual(productB_Name, bestOffer.Name);
+        }
+
+        [TestMethod()]
+        public void GetOffers_ProductAIsTheBestOffer_IfConsumptionIs6000()
+        {
+            decimal consumption = 6000;
+            var model = InitializeTariffModel();
+            var offers = model.GetOffers(consumption);
+            var bestOffer = offers[0];
+            Assert.AreEqual(productA_Name, bestOffer.Name);
+        }
+
+ 
+    }
+}
