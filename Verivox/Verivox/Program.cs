94db62a4c7ae63f0aa49589e5d8bb8f61d6ac50d
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-12-31 10:05:18 +0300

    Added console app program

--- Verivox/Verivox/Program.cs
+++ Verivox/Verivox/Program.cs
@@ -10,6 +10,45 @@ namespace Verivox
     {
         static void Main(string[] args)
         {
+            TariffModel model = new TariffModel();
+
+            //Product productA = new ProductA("basic electricity tariff");
+            //Product productB = new ProductB("Packaged tariff");
+
+            Product productA = new ProductA();
+            Product productB = new ProductB();
+
+            model.AddProduct(productA);
+            model.AddProduct(productB);
+
+            decimal consumption = 3500M;
+            Console.WriteLine($"Consumption : {consumption}");
+
+            var offers = model.GetOffers(consumption);
+            foreach (var offer in offers)
+            {
+                Console.WriteLine($"Name : {offer.Name}, TotalCost : {offer.TotalCost}");
+            }
+
+            consumption = 4500M;
+            Console.WriteLine($"Consumption : {consumption}");
+
+            offers = model.GetOffers(consumption);
+            foreach (var offer in offers)
+            {
+                Console.WriteLine($"Name : {offer.Name}, TotalCost : {offer.TotalCost}");
+            }
+
+            consumption = 6000M;
+            Console.WriteLine($"Consumption : {consumption}");
+
+            offers = model.GetOffers(consumption);
+            foreach (var offer in offers)
+            {
+                Console.WriteLine($"Name : {offer.Name}, TotalCost : {offer.TotalCost}");
+            }
+
+            Console.WriteLine();
         }
     }
 }
